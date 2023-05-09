06b06d5252d3a040ada2a07fbb5901f3992c7860
kong-king <kly_ing@163.com>
2023-05-09 13:28:01 +0800

    add

--- code/数组中第K个最大元素.go
+++ code/数组中第K个最大元素.go
@@ -1,33 +1,100 @@
 package code
 
 func findKthLargest(nums []int, k int) int {
-	if len(nums) < k {
-		return -1
-	}
-
-	quickSort02(nums, 0, len(nums)-1)
+	TopKSplit(nums, len(nums)-k, 0, len(nums)-1)
 	return nums[len(nums)-k]
 }
 
-func quickSort02(arr []int, start, end int) {
-	if start >= end {
-		return
+func Parition(nums []int, start, stop int) int {
+	if start >= stop {
+		return -1
 	}
-
-	var i, j = start, end
-	var base = arr[start]
-
-	for i < j {
-		for i < j && arr[j] >= base {
-			j--
+	pivot := nums[start]
+	l, r := start, stop
+	for l < r {
+		for l < r && nums[r] >= pivot {
+			r--
 		}
-
-		for i < j && arr[i] <= base {
-			i++
+		nums[l] = nums[r]
+		for l < r && nums[l] < pivot {
+			l++
 		}
-		arr[i], arr[j] = arr[j], arr[i]
+		nums[r] = nums[l]
 	}
-	arr[start], arr[i] = arr[i], arr[start]
-	quickSort02(arr, start, i-1)
-	quickSort02(arr, i+1, end)
+	// 循环结束，l与r相等
+	// 确定基准元素pivot在数组中的位置
+	nums[l] = pivot
+	return l
 }
+
+func TopKSplit(nums []int, k, start, stop int) {
+	if start < stop {
+		index := Parition(nums, start, stop)
+		if index == k {
+			return
+		} else if index < k {
+			TopKSplit(nums, k, index+1, stop)
+		} else {
+			TopKSplit(nums, k, start, index-1)
+		}
+	}
+}
+
+//func findKthLargest(nums []int, k int) int {
+//	max := nums[0]
+//	min := nums[0]
+//	for _, value := range nums {
+//		if value < min {
+//			min = value
+//		}
+//		if value > max {
+//			max = value
+//		}
+//	}
+//	//创建一个数组， 实现哈希表的功能， 记录每一个数出现的个数，然后倒叙遍历，用k减去前top出现的次数，得到第K大的数
+//	//注意：count数组里面记录的是具体的数的索引值，而非数的真实差值
+//	count := make([]int, max-min+1)
+//	for _, num := range nums {
+//		count[num-min]++
+//	}
+//	for i := max - min; i >= 0; i-- {
+//		k -= count[i]
+//		if k <= 0 {
+//			return i + min
+//		}
+//	}
+//	return nums[0]
+//}
+
+//
+//func findKthLargest(nums []int, k int) int {
+//	if len(nums) < k {
+//		return -1
+//	}
+//
+//	quickSort02(nums, 0, len(nums)-1)
+//	return nums[len(nums)-k]
+//}
+//
+//func quickSort02(arr []int, start, end int) {
+//	if start >= end {
+//		return
+//	}
+//
+//	var i, j = start, end
+//	var base = arr[start]
+//
+//	for i < j {
+//		for i < j && arr[j] >= base {
+//			j--
+//		}
+//
+//		for i < j && arr[i] <= base {
+//			i++
+//		}
+//		arr[i], arr[j] = arr[j], arr[i]
+//	}
+//	arr[start], arr[i] = arr[i], arr[start]
+//	quickSort02(arr, start, i-1)
+//	quickSort02(arr, i+1, end)
+//}
--- code/螺旋矩阵.go
+++ code/螺旋矩阵.go
@@ -10,41 +10,57 @@ func spiralOrder(matrix [][]int) []int {
 	}
 	x := len(matrix[0])
 
-	var visit [][]bool
-	for j := 0; j < y; j++ {
-		visit = append(visit, make([]bool, x))
-	}
 	var res []int
-	spiralOrderDfs(matrix, 0, 0, visit, &res, right)
+	var visit = make([][]bool, y)
+	for i := 0; i < y; i++ {
+		visit[i] = make([]bool, x)
+	}
+
+	spiralOrderDFS(matrix, 0, 0, visit, &res, right)
 	return res
 }
 
-type orderType uint8
+type action int
 
 const (
-	left orderType = iota
-	down
+	left action = iota
 	right
 	up
+	down
 )
 
-func spiralOrderDfs(matrix [][]int, i, j int, visit [][]bool, res *[]int, order orderType) {
-	y := len(matrix)
-	x := len(matrix[0])
+/*
+matrix 与y轴、x轴的关系
+0—— —— —— —— > x轴
+|00 01 02 03
+|10 11 12 13
+|20 21 22 23
+|30 31 32 33
+V
+y轴
+*/
+// action是方向。left的下一步只有left和down。 down、right、up依次类推。做dfs遍历
+func spiralOrderDFS(matrix [][]int, i, j int, visit [][]bool, res *[]int, act action) {
+	var y = len(matrix)
+	var x = len(matrix[0])
+
 	if i < 0 || i >= x || j < 0 || j >= y || visit[j][i] {
 		return
 	}
-
 	*res = append(*res, matrix[j][i])
 	visit[j][i] = true
-
-	if order == up {
-		spiralOrderDfs(matrix, i, j-1, visit, res, up)
-		spiralOrderDfs(matrix, i+1, j, visit, res, right)
-	} else {
-		spiralOrderDfs(matrix, i+1, j, visit, res, right)
-		spiralOrderDfs(matrix, i, j+1, visit, res, down)
-		spiralOrderDfs(matrix, i-1, j, visit, res, left)
-		spiralOrderDfs(matrix, i, j-1, visit, res, up)
+	switch act {
+	case right:
+		spiralOrderDFS(matrix, i+1, j, visit, res, right)
+		spiralOrderDFS(matrix, i, j+1, visit, res, down)
+	case down:
+		spiralOrderDFS(matrix, i, j+1, visit, res, down)
+		spiralOrderDFS(matrix, i-1, j, visit, res, left)
+	case left:
+		spiralOrderDFS(matrix, i-1, j, visit, res, left)
+		spiralOrderDFS(matrix, i, j-1, visit, res, up)
+	case up:
+		spiralOrderDFS(matrix, i, j-1, visit, res, up)
+		spiralOrderDFS(matrix, i+1, j, visit, res, right)
 	}
 }
